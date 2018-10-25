435277a3c1c6cabc035c71276a20cb1982ed3eab
Kareem <kareemzx@hotmail.com>
2018-10-24 20:40:30 -0400

    added comments

--- test.py
+++ test.py
@@ -1,23 +1,30 @@
+## Imported Modules
+
 from sense_hat import SenseHat
 from time import sleep
 from firebase import firebase
+from flask import flask
 
+## Declared variables
 
+##Module variables
 sense = SenseHat()
 sense.clear()
 firebase = firebase.FirebaseApplication('https://project502-4a209.firebaseio.com/kareem',None)
 
-
-
+## Sensor variables
 p = 0.0
 t = 0.0
 h = 0.0
 
+## Colour variables
 blue = (0,0,255)
 red = (255,0,0)
 
+## Message variable
 message = "Temperature: " + str(t) + " Pressure: " + str(p) + " Humidity: " + str(h)
 
+## GetData function defined
 def getData():
 	p = sense.get_pressure()
 	t = sense.get_temperature()
@@ -30,11 +37,13 @@ def getData():
 	sense.clear()
 	firebase.post('/kareem', {'Temperature': str(t), 'Pressure': str(p), 'Humidity': str(h)})
 
+## Displays program starting message
 sense.show_message("Program Starting in 3 2 1", scroll_speed = 0.09, text_colour = red)
 
+## Program running Sequence
 while True:
 	getData()
-	sleep(5)
+	sleep(10)
 
 
 
